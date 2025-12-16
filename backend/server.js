454a0c50097bbfed9d2e944049697d89b65360cc
bikash1376 <bikash13763@gmail.com>
2025-12-17 01:33:01 +0530

    resolving cors

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://prepara-eight.vercel.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
